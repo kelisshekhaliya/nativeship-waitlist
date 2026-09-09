430a05d1bd97c62995b1edf4d7aa0db279a2c344
keli-spatel <kelisshekhaliya@gmail.com>
2026-09-09 20:56:22 +0530

    Add suppressHydrationWarning to body element in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${gabarito.variable} ${inter.variable}`}>
-      <body>{children}</body>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   );
 }
